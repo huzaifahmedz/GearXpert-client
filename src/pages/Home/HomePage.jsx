d9e795c63c721d1bdd38af1ed0cba502ce587939
Huzaif Ahmed <dev.huzaif@gmail.com>
2025-10-27 13:18:24 +0530

    Comment out unused component imports in HomePage

--- src/pages/Home/HomePage.jsx
+++ src/pages/Home/HomePage.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import HeroSection from "../../components/herosection/HeroSection";
 import AutomobileInfo from "../../components/seconderySection/AutomobileInfo";
-import Navigation from "../../components/Navigation/Navigation";
+// import Navigation from "../../components/Navigation/Navigation";
 import ImageSlider from "../../components/slider/ImageSlider";
-import Testimonial from "../../components/testimonials/Testimonial.Jsx";
+import Testimonial from "../../components/testimonials/Testimonial";
 import ChooseUs from "../../components/whyChoose/ChooseUs";
 import Marque from "../../components/MarqueLine/Marque";
 import Services from "../../components/Serivices/Services";
-import Footer from "../../components/footer/Footer";
+// import Footer from "../../components/footer/Footer";
 
 const HomePage = () => {
   return <>
